0b92cbca2ba18c95c29127561deed113dbd55cfa
sinss0000 <sinss0000@gmail.com>
2013-10-21 12:32:55 +0800

    change mesage

--- LCPickerViewSample/ViewController.m
+++ LCPickerViewSample/ViewController.m
@@ -38,7 +38,7 @@
     self.maskView = [[UIView alloc] initWithFrame:self.view.bounds];
     [_maskView setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0]];
     [self.view addSubview:_maskView];
-    LCTableViewPickerControl *selectAgeView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, kPickerControlWidth, kPickerControlAgeHeight) title:@"Please pick a item" value:_pickValue items:@[@"item1",@"item2",@"item3",@"item4",@"item5",@"item6"]];
+    LCTableViewPickerControl *selectAgeView = [[LCTableViewPickerControl alloc] initWithFrame:CGRectMake(0, self.view.frame.size.height, kPickerControlWidth, kPickerControlAgeHeight) title:@"Please pick an item" value:_pickValue items:@[@"item1",@"item2",@"item3",@"item4",@"item5",@"item6"]];
     [selectAgeView setDelegate:self];
     
     
